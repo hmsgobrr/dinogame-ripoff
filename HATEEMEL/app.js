d8fc870b302f5218f93bb057cc872e513433a7f8
HmsGoBrr <66516230+HmsGoBrr@users.noreply.github.com>
2020-06-09 08:28:35 +0700

    Add files via upload
    Japaskripp Gaem kompliet

--- HATEEMEL/app.js
+++ HATEEMEL/app.js
@@ -1,10 +1,62 @@
-const character = document.getElementById("character");
-const block = document.getElementById("block");
+//? Elements
+const menu = document.getElementById("start");
+const play = document.getElementById("play");
+const aboutb = document.getElementById("aboutb");
+const about = document.getElementById("about");
+const backb = document.getElementById("back");
 
+const character = document.getElementById("character");
+const charImg = document.getElementById("charimg");
+const block = document.getElementById("block");
+const game = document.getElementById("game");
+const score = document.getElementById("score");
+const dedMenu = document.getElementById("dedMenu");
+const retry = document.getElementById("retry");
+const menub = document.getElementById("menu");
+var scc;
+
+const jSfx = document.getElementById("jumpSfx");
+const dSfx = document.getElementById("dedSfx");
+
+//? Start Menu
+window.onload = () => {
+    game.style.display = "none";
+    about.style.display = "none";
+    menu.style.display = "block";
+};
+play.addEventListener("click", () => {
+    scc = 0;
+    menu.style.display = "none";
+    game.style.display = "block";
+    dedMenu.style.display = "none";
+    setTimeout(() => {
+        block.classList.add("battack");
+    }, 1000);
+});
+aboutb.addEventListener("click", () => {
+    game.style.display = "none";
+    about.style.display = "block";
+    menu.style.display = "none";
+});
+
+// //? Score
+// block.addEventListener("animationstart", () => {
+//     scc = 0;
+//     setInterval(() => {
+//         if (block.classList == "battack") {
+//             scc++;
+//             score.innerHTML = "Score: " + scc;
+//         }
+//     }, 1000);
+// });
+
+//? Jump functions
 window.addEventListener("keydown", jump, false);
+game.addEventListener("click", jumpc);
 
 function jump(key) {
-    if (key.keyCode == 32) {
+    if (key.keyCode == 32 || key.keyCode == 38) {
+        jSfx.play();
         if (character.classList != "animate") {
             character.classList.add("animate");
         }
@@ -13,7 +65,17 @@ function jump(key) {
         }, 500);
     }
 }
+function jumpc() {
+    jSfx.play();
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setTimeout(() => {
+        character.classList.remove("animate");
+    }, 500);
+}
 
+//? Check if the player is dead
 const checkDead = setInterval(() => {
     var characterTop = parseInt(
         window.getComputedStyle(character).getPropertyValue("top")
@@ -22,8 +84,38 @@ const checkDead = setInterval(() => {
         window.getComputedStyle(block).getPropertyValue("left")
     );
     if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
-        block.style.animation = "none";
+        dSfx.play();
+        dedMenu.style.display = "block";
+        charImg.src = "charImg/characterDed.png";
+        block.classList.remove("battack");
         block.style.display = "none";
-        alert("lol ur ded");
     }
 }, 10);
+
+//? Retry
+retry.addEventListener("click", () => {
+    scc = 0;
+    menu.style.display = "none";
+    game.style.display = "block";
+    dedMenu.style.display = "none";
+    block.style.display = "block";
+    charImg.src = "charImg/character.png";
+    setTimeout(() => {
+        block.classList.add("battack");
+    }, 1000);
+});
+
+//? Go to menu
+function reload() {
+    location.reload();
+}
+menub.addEventListener("click", reload);
+backb.addEventListener("click", reload);
+/*
+TODO: about menu
+TODO: navbar
+TODO: favicon
+TODO: publish
+
+! Score system error ;-;
+*/
